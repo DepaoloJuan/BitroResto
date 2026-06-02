ce14030238120fe576e3f0ded56e65d4fd451ae3
carmen-cespedes <cespedescarmenc@gmail.com>
2026-06-02 00:41:49 -0300

    agregado de funcion constante para lector

---
+++ src/app/core/services/camara.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Injectable({ providedIn: 'root' })
+export class CamaraService {
+  async tomarFoto(): Promise<string> {
+    const foto = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+    });
+    return foto.dataUrl ?? '';
+  }
+}
